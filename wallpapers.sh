d73896e943c77f6dccc0c906e2bc115138dd19c3
geekfish69 <a_pang@outlook.com>
2022-07-05 19:16:31 +0530

    Initial push

---
+++ wallpapers.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd ~/.themes/Mojave-dark/gnome-shell/assets
+mv background.png ~/.themes/Mojave-dark/gnome-shell/assets/background6.png
+mv background2.png ~/.themes/Mojave-dark/gnome-shell/assets/background.png
+mv background3.png ~/.themes/Mojave-dark/gnome-shell/assets/background2.png
+mv background4.png ~/.themes/Mojave-dark/gnome-shell/assets/background3.png
+mv background5.png ~/.themes/Mojave-dark/gnome-shell/assets/background4.png
+mv background6.png ~/.themes/Mojave-dark/gnome-shell/assets/background5.png
+
+
+cd ~/.config
+mv background ./background6
+mv background2 ./background
+mv background3 ./background2
+mv background4 ./background3
+mv background5 ./background4
+mv background6 ./background5
+
+exit
+
+
